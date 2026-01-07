9e5852fdeb68da240c0de981db9351c0bc3e77b3
joshualarido <joshualarido@gmail.com>
2026-01-07 12:01:10 +0700

    updated main.py to download model

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+import os
 import joblib
 import numpy as np
 import pandas as pd
-from pydantic import BaseModel
-from typing import List
-import os
+import requests
+from pathlib import Path
 from dotenv import load_dotenv
-from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
-# LOAD APP
 app = FastAPI(
     title="Floodsense AI Service",
     description="AI inference service for flood prediction using trained ML model",
@@ -25,48 +27,57 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL_PATH = os.getenv("MODEL_PATH")
+MODEL_URL = os.getenv("MODEL_URL")
+MODEL_LOCAL_PATH = "app/model/flood_model_v1.joblib"
 
-if MODEL_PATH is None:
-    raise RuntimeError("MODEL_PATH environment variable not set")
+if MODEL_URL is None:
+    raise RuntimeError("MODEL_URL environment variable not set")
 
-bundle = joblib.load(MODEL_PATH)
+Path("app/model").mkdir(parents=True, exist_ok=True)
+
+if not Path(MODEL_LOCAL_PATH).exists():
+    print("Downloading model from Hugging Face...")
+    response = requests.get(MODEL_URL, stream=True)
+    response.raise_for_status()
+
+    with open(MODEL_LOCAL_PATH, "wb") as f:
+        for chunk in response.iter_content(chunk_size=32768):
+            if chunk:
+                f.write(chunk)
+
+    print("Model downloaded successfully.")
+
+bundle = joblib.load(MODEL_LOCAL_PATH)
 pipeline = bundle["pipeline"]
 feature_columns = bundle["feature_columns"]
+
+class PredictionInput(BaseModel):
+    features: List[float]
 
 @app.get("/")
 def root():
     return {
-        "service": "Flood Risk AI Service",
+        "service": "Floodsense AI Service",
         "status": "running"
     }
 
 @app.get("/health")
 def health_check():
-        return {"status": "ok"}
-
-class PredictionInput(BaseModel):
-        features: List[float]
-
-FEATURE_NAMES = [
-    "jrc_perm_water",
-    "precip_1d",
-    "precip_3d",
-    "NDVI",
-    "NDWI",
-    "landcover",
-    "elevation",
-    "slope",
-    "aspect",
-    "upstream_area",
-    "TWI"
-]
+    return {"status": "ok"}
 
 @app.post("/predict")
 def predict(data: PredictionInput):
+
     if len(data.features) != len(feature_columns):
         return {
-            "error": f"Expected {len(feature_columns)} features, got {len(data.features)}"
+            "error": f"Expected {len(feature_columns)} features, got {len(data.features)}",
+            "expected_order": feature_columns
+        }
+
+    input_array = np.array(data.features)
+    if not np.isfinite(input_array).all():
+        return {
+            "error": "Input contains NaN or infinite values"
         }
 
     input_df = pd.DataFrame(
