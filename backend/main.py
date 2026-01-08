2fe51fecd0ced5ffa73ca01cdae484474ba83520
joshualarido <joshualarido@gmail.com>
2026-01-08 10:36:59 +0700

    added cors middleware for gh pages

--- backend/main.py
+++ backend/main.py
@@ -26,8 +26,9 @@ app = FastAPI(title="Floodsense Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", 
+        "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://joshualarido.github.io/floodsense/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
